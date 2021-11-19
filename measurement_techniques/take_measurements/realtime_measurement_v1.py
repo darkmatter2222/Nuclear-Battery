b4ef70c4d30e1e3d25a83b54a1f614b75dd9e743
ryan susman <ryansusman@gmail.com>
2021-11-19 14:57:55 -0600

    add pause to voltage colection

---
+++ measurement_techniques/take_measurements/realtime_measurement_v1.py
@@ -0,0 +1,75 @@
+import RPi.GPIO as GPIO
+import time
+import datetime
+import threading
+import Adafruit_ADS1x15
+from pymongo import MongoClient
+import pymongo
+import os
+import json
+from tqdm import tqdm
+from dotenv import load_dotenv
+
+from pathlib import Path
+
+GPIO.setmode(GPIO.BCM)
+voltage_measurement_pin = 26
+current_measurement_pin = 6
+reset_pin = 19
+GPIO.setup(voltage_measurement_pin, GPIO.OUT)
+GPIO.setup(current_measurement_pin, GPIO.OUT)
+
+tritium_cell_number = input("What tritium cell number is this?")
+solar_cell_number = input("What solar cell number is this?")
+adc = Adafruit_ADS1x15.ADS1115()
+
+# Choose a gain of 1 for reading voltages from 0 to 4.09V.
+# Or pick a different gain to change the range of voltages that are read:
+#  - 2/3 = +/-6.144V
+#  -   1 = +/-4.096V
+#  -   2 = +/-2.048V
+#  -   4 = +/-1.024V
+#  -   8 = +/-0.512V
+#  -  16 = +/-0.256V
+# See table 3 in the ADS1015/ADS1115 datasheet for more info on gain.
+GAIN = 1
+max_list_length = 30
+
+voltage_list = []
+current_list = []
+
+
+def perform_measurement():
+    global voltage_list
+    global current_list
+
+    channel = 0
+    value = adc.read_adc(channel, gain=GAIN)
+    voltage = round((4.096 * value) / 32767, 6)
+    voltage_list.append(voltage)
+    if len(voltage_list) > max_list_length:
+        del max_list_length[0]
+
+    channel = 1
+    value = adc.read_adc(channel, gain=GAIN)
+    voltage = round((4.096 * value) / 32767, 6)
+    amperage = voltage / 100000
+    current_list.append(amperage)
+    if len(current_list) > max_list_length:
+        del voltage_list[0]
+
+
+
+GPIO.output(voltage_measurement_pin, GPIO.HIGH)
+
+while True:
+    try:
+        print("Beginning...")
+        time.sleep(0.5)
+        perform_measurement()
+        print(voltage_list)
+    except:
+        lol = 1
+
+GPIO.output(voltage_measurement_pin, GPIO.LOW)
+
