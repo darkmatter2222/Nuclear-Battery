d9e12dccd2b5bef8bb7c52a34d16dd5a2ad18aa7
ryan susman <ryansusman@gmail.com>
2021-11-22 11:13:38 -0600

    checkpoint

--- measurement_techniques/take_measurements/optics/spectro_collect.py
+++ measurement_techniques/take_measurements/optics/spectro_collect.py
@@ -2,5 +2,5 @@ import os
 import time
 
 for i in range(50):
-        os.system(f'libcamera-still -o /home/pi/Desktop/Spectrum1/test.jpg --shutter 30000000 --gain 100 --awbgains 1,1 --immediate')
+        os.system(f'libcamera-still -o /home/pi/Desktop/Spectrum1/{i}.jpg --shutter 30000000 --gain 100 --awbgains 1,1 --immediate')
         time.sleep(10)
