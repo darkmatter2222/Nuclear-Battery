9f625b67fa33654b38e456eefc13bbb19afa18dd
ryan susman <ryansusman@gmail.com>
2021-12-12 20:37:16 -0600

    spectrometer

--- measurement_techniques/take_measurements/optics/light_and_spectro_collect.py
+++ measurement_techniques/take_measurements/optics/light_and_spectro_collect.py
@@ -11,69 +11,57 @@ i2c = board.I2C()  # uses board.SCL and board.SDA
 sensor = AS7341(i2c)
 veml7700 = adafruit_veml7700.VEML7700(i2c)
 
-result = {'lux': 0,
+result = {'lux': veml7700.lux,
           'target_wavelengths': {
               '415': {
                   'color_name': 'Violet',
-                  'color_intensity': 0,
+                  'color_intensity': sensor.channel_415nm,
               },
               '445': {
                   'color_name': 'Indigo',
-                  'color_intensity': 0,
+                  'color_intensity': sensor.channel_445nm,
               },
               '480': {
                   'color_name': 'Blue',
-                  'color_intensity': 0,
+                  'color_intensity': sensor.channel_480nm,
               },
               '515': {
                   'color_name': 'Cyan',
-                  'color_intensity': 0,
+                  'color_intensity': sensor.channel_515nm,
               },
               '555': {
                   'color_name': 'Green',
-                  'color_intensity': 0,
+                  'color_intensity': sensor.channel_555nm,
               },
               '590': {
                   'color_name': 'Yellow',
-                  'color_intensity': 0,
+                  'color_intensity': sensor.channel_590nm,
               },
               '630': {
                   'color_name': 'Orange',
-                  'color_intensity': 0,
+                  'color_intensity': sensor.channel_630nm,
               },
               '680': {
                   'color_name': 'Red',
-                  'color_intensity': 0,
+                  'color_intensity': sensor.channel_680nm,
               },
           },
           'special_measurements': {
               'Clear': {
                   'color_name': 'Clear',
-                  'color_intensity': 0,
+                  'color_intensity': sensor.channel_clear,
               },
               'NIR': {
                   'color_name': 'Near-IR',
-                  'color_intensity': 0,
+                  'color_intensity': sensor.channel_nir,
               },
+          },
+          'sensor_config': {
+              'adafruit_VEML7700': {
+                  'integration_time_value': veml7700.integration_time_value(),
+                  'gain_value': veml7700.gain_value()
+              }
           }
           }
-
-
-def bar_graph(read_value):
-    scaled = int(read_value / 1000)
-    return "[%5d] " % read_value + (scaled * "*")
-
-# begin taking measurement
-result['lux'] = veml7700.lux
-result['target_wavelengths']['415']['color_intensity'] = sensor.channel_415nm
-result['target_wavelengths']['445']['color_intensity'] = sensor.channel_445nm
-result['target_wavelengths']['480']['color_intensity'] = sensor.channel_480nm
-result['target_wavelengths']['515']['color_intensity'] = sensor.channel_515nm
-result['target_wavelengths']['555']['color_intensity'] = sensor.channel_555nm
-result['target_wavelengths']['590']['color_intensity'] = sensor.channel_590nm
-result['target_wavelengths']['630']['color_intensity'] = sensor.channel_630nm
-result['target_wavelengths']['680']['color_intensity'] = sensor.channel_680nm
-result['special_measurements']['Clear']['color_intensity'] = sensor.channel_clear
-result['special_measurements']['NIR']['color_intensity'] = sensor.channel_nir
 
 print(json.dumps(result, indent=1))
